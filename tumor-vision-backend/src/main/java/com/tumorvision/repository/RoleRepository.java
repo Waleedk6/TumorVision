bde0d072d1bd0b50dddc5baf902ade3493e59f5f
K-Waleed <waleedhigh369@gmail.com>
2025-06-06 00:05:23 +0500

    commit

--- tumor-vision-backend/src/main/java/com/tumorvision/repository/RoleRepository.java
+++ tumor-vision-backend/src/main/java/com/tumorvision/repository/RoleRepository.java
@@ -1,11 +1,13 @@
 package com.tumorvision.repository;
 
 import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import com.tumorvision.model.ERole;
 import com.tumorvision.model.Role;
 
-
-public interface RoleRepository extends MongoRepository<Role, String> {
+public interface RoleRepository extends MongoRepository<Role, String> 
+{
     Optional<Role> findByName(ERole name);
 }

1c3ec0430a39f9fdcf8ff92fff257097bee30af8
K-Waleed <waleedhigh369@gmail.com>
2025-06-17 21:43:28 +0500

    commit backend

--- tumor-vision-backend/src/main/java/com/tumorvision/repository/RoleRepository.java
+++ tumor-vision-backend/src/main/java/com/tumorvision/repository/RoleRepository.java
@@ -3,7 +3,6 @@ package com.tumorvision.repository;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 import com.tumorvision.model.ERole;
 import com.tumorvision.model.Role;
 

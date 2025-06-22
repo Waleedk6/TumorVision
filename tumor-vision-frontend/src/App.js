f2ff12ef935d6375c9d0d94b10c73538d28c4232
K-Waleed <waleedhigh369@gmail.com>
2025-06-22 21:23:49 +0500

    commit

--- tumor-vision-frontend/src/App.js
+++ tumor-vision-frontend/src/App.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import AdminPanel from './pages/AdminPanel';
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import Patients from './pages/Patients';
-import UploadArea from './pages/UploadArea';
-import Settings from './pages/Settings';
-import './styles/colors.css';
-import './styles/landing.css';
-import './styles/utilities.css';
-import './styles/animations.css';
-
-=======
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -34,7 +17,6 @@ import "./styles/colors.css";
 import "./styles/landing.css";
 import "./styles/utilities.css";
 import "./styles/animations.css";
->>>>>>> 9994d633cb4f6f1cc2ea26a73aad759168b0844d
 
 function App() {
   return (

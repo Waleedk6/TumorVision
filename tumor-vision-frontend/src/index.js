479146245205561801c7de40e2690324ec730b2a
sibte0340 <119343295+sibte0340@users.noreply.github.com>
2025-09-11 18:00:23 +0500

    commit

--- tumor-vision-frontend/src/index.js
+++ tumor-vision-frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import 'react-calendar/dist/Calendar.css';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 

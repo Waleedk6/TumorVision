043c17adca668f783ceddb24ffa84635bedcaff1
sibte0340 <119343295+sibte0340@users.noreply.github.com>
2025-09-11 17:59:51 +0500

    commit

--- tumor-vision-frontend/src/index.js
+++ tumor-vision-frontend/src/index.js
@@ -8,6 +8,7 @@ import 'react-calendar/dist/Calendar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
